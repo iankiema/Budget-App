2dc5bab3d1a14eeb7744c40b4e6d5d72762874a2
iankiema <kiemaian.ik@gmail.com>
2023-11-30 13:18:06 +0300

    Configur devise, routes and gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  authenticated :user do
+    root "categories#index", as: :authenticated_root
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  unauthenticated do
+    root "pages#splash", as: :unauthenticated_root
+  end
+
+  devise_for :users
+
+  resources :categories do
+    resources :expenditures, only: [:index, :new, :create, :destroy]
+  end
 end
